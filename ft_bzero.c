dacf43284b5026c8acb0f3dc056d17df9d862c37
Alois Marti <kai-iou@mbp-de-alois.home>
2024-10-27 19:41:15 +0100

    libft

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,29 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kai-iou <kai-iou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/24 23:06:14 by kai-iou           #+#    #+#             */
-/*   Updated: 2024/10/26 13:28:18 by kai-iou          ###   ########.fr       */
+/*   Created: 2024/10/27 19:39:14 by kai-iou           #+#    #+#             */
+/*   Updated: 2024/10/27 19:40:15 by kai-iou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t n)
-{
-	size_t	i;
-	unsigned char *cv2;
-
-	i = 0;
-	cv2 = (unsigned char *)s;
-
-	while (i != n)
-	{
-		cv2[i] = (unsigned char)c;
-		i++;
-	}
-	return (s);
-}
